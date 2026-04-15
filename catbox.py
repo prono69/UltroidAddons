65761745e0d195174c78f85867f4bbfd4c6e50d1
prono69 <faltu9851@gmail.com>
2026-04-15 12:29:39 +0530

    update catbox.py

--- catbox.py
+++ catbox.py
@@ -11,6 +11,7 @@ import os
 
 import aiohttp
 from catbox import CatboxUploader
+from pyUltroid.fns.misc import upload_to_qu
 
 from . import udB, ultroid_cmd
 
@@ -29,22 +30,6 @@ async def upload_to_envs(file_path):
                 return url_
             else:
                 return f"Error: {response.status} - {await response.text()}"
-
-
-async def upload_to_qu(file_path, url="https://qu.ax/upload.php"):
-    async with aiohttp.ClientSession() as session:
-        with open(file_path, "rb") as f:
-            files = {"files[]": f.read()}  # Read file content asynchronously
-            data = {"expiry": "365"}  # Add expiry parameter
-        async with session.post(url, data=files, params=data) as response:
-            try:
-                response_json = await response.json()
-                file_url = response_json.get("files", [{}])[0].get("url", "URL not found")
-                return file_url
-            except aiohttp.ContentTypeError:
-                print("Response is not in JSON format")
-    return None
-
 
 
 @ultroid_cmd(pattern="catb ?(.*)$")
